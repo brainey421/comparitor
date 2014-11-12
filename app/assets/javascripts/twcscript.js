c9425cb3133bda5f17cb382528c7fb6a2e1c2866
brainey421 <raineyb@purdue.edu>
2014-11-12 18:47:29 -0500

    Add keyboard shortcuts.

--- app/assets/javascripts/twcscript.js
+++ app/assets/javascripts/twcscript.js
@@ -9,20 +9,20 @@ $(document).ready(function() {
 			$('[name=rank1]').val('1');
 		
 			$('div#item1').html('<p>First!</p>');
-			$('div#item2').html('<button id="tie2" type="button" class="btn btn-primary">tie for first</button> <button id="rank2" type="button" class="btn btn-primary">rank second</button>');
-			$('div#item3').html('<button id="tie3" type="button" class="btn btn-primary">tie for first</button> <button id="rank3" type="button" class="btn btn-primary">rank second</button>');
+			$('div#item2').html('<button id="tie2" accesskey="w" type="button" class="btn btn-primary">tie for first</button> <button id="rank2" accesskey="2" type="button" class="btn btn-primary">rank second</button>');
+			$('div#item3').html('<button id="tie3" accesskey="e" type="button" class="btn btn-primary">tie for first</button> <button id="rank3" accesskey="3" type="button" class="btn btn-primary">rank second</button>');
 		}
 		else if (rank2 == '1' && rank3 == '0') {
 			$('[name=rank1]').val('2');
 			
 			$('div#item1').html('<p>Second!</p>');
-			$('div#item3').html('<button id="tie3" type="button" class="btn btn-primary">tie for second</button> <button id="rank3" type="button" class="btn btn-primary">rank third</button>');
+			$('div#item3').html('<button id="tie3" accesskey="e" type="button" class="btn btn-primary">tie for second</button> <button id="rank3" accesskey="3" type="button" class="btn btn-primary">rank third</button>');
 		}
 		else if (rank2 == '0' && rank3 == '1') {
 			$('[name=rank1]').val('2');
 			
 			$('div#item1').html('<p>Second!</p>');
-			$('div#item2').html('<button id="tie2" type="button" class="btn btn-primary">tie for second</button> <button id="rank2" type="button" class="btn btn-primary">rank third</button>');
+			$('div#item2').html('<button id="tie2" accesskey="w" type="button" class="btn btn-primary">tie for second</button> <button id="rank2" accesskey="2" type="button" class="btn btn-primary">rank third</button>');
 		}
 		else if (rank2 == '1' && rank3 == '1') {
 			$('[name=rank1]').val('2');
@@ -47,20 +47,20 @@ $(document).ready(function() {
 		if (rank1 == '0' && rank3 == '0') {
 			$('[name=rank2]').val('1');
 		
-			$('div#item1').html('<button id="tie1" type="button" class="btn btn-primary">tie for first</button> <button id="rank1" type="button" class="btn btn-primary">rank second</button>');
+			$('div#item1').html('<button id="tie1" accesskey="q" type="button" class="btn btn-primary">tie for first</button> <button id="rank1" accesskey="1" type="button" class="btn btn-primary">rank second</button>');
 			$('div#item2').html('<p>First!</p>');
-			$('div#item3').html('<button id="tie3" type="button" class="btn btn-primary">tie for first</button> <button id="rank3" type="button" class="btn btn-primary">rank second</button>');
+			$('div#item3').html('<button id="tie3" accesskey="e" type="button" class="btn btn-primary">tie for first</button> <button id="rank3" accesskey="3" type="button" class="btn btn-primary">rank second</button>');
 		}
 		else if (rank1 == '1' && rank3 == '0') {
 			$('[name=rank2]').val('2');
 			
 			$('div#item2').html('<p>Second!</p>');
-			$('div#item3').html('<button id="tie3" type="button" class="btn btn-primary">tie for second</button> <button id="rank3" type="button" class="btn btn-primary">rank third</button>');
+			$('div#item3').html('<button id="tie3" accesskey="e" type="button" class="btn btn-primary">tie for second</button> <button id="rank3" accesskey="3" type="button" class="btn btn-primary">rank third</button>');
 		}
 		else if (rank1 == '0' && rank3 == '1') {
 			$('[name=rank2]').val('2');
 			
-			$('div#item1').html('<button id="tie1" type="button" class="btn btn-primary">tie for second</button> <button id="rank1" type="button" class="btn btn-primary">rank third</button>');
+			$('div#item1').html('<button id="tie1" accesskey="q" type="button" class="btn btn-primary">tie for second</button> <button id="rank1" accesskey="1" type="button" class="btn btn-primary">rank third</button>');
 			$('div#item2').html('<p>Second!</p>');
 		}
 		else if (rank1 == '1' && rank3 == '1') {
@@ -86,20 +86,20 @@ $(document).ready(function() {
 		if (rank1 == '0' && rank2 == '0') {
 			$('[name=rank3]').val('1');
 		
-			$('div#item1').html('<button id="tie1" type="button" class="btn btn-primary">tie for first</button> <button id="rank1" type="button" class="btn btn-primary">rank second</button>');
-			$('div#item2').html('<button id="tie2" type="button" class="btn btn-primary">tie for first</button> <button id="rank2" type="button" class="btn btn-primary">rank second</button>');
+			$('div#item1').html('<button id="tie1" accesskey="q" type="button" class="btn btn-primary">tie for first</button> <button id="rank1" accesskey="1" type="button" class="btn btn-primary">rank second</button>');
+			$('div#item2').html('<button id="tie2" accesskey="w" type="button" class="btn btn-primary">tie for first</button> <button id="rank2" accesskey="2" type="button" class="btn btn-primary">rank second</button>');
 			$('div#item3').html('<p>First!</p>');
 		}
 		else if (rank1 == '1' && rank2 == '0') {
 			$('[name=rank3]').val('2');
 			
-			$('div#item2').html('<button id="tie2" type="button" class="btn btn-primary">tie for second</button> <button id="rank2" type="button" class="btn btn-primary">rank third</button>');
+			$('div#item2').html('<button id="tie2" accesskey="w" type="button" class="btn btn-primary">tie for second</button> <button id="rank2" accesskey="2" type="button" class="btn btn-primary">rank third</button>');
 			$('div#item3').html('<p>Second!</p>');
 		}
 		else if (rank1 == '0' && rank2 == '1') {
 			$('[name=rank3]').val('2');
 			
-			$('div#item1').html('<button id="tie1" type="button" class="btn btn-primary">tie for second</button> <button id="rank1" type="button" class="btn btn-primary">rank third</button>');
+			$('div#item1').html('<button id="tie1" accesskey="q" type="button" class="btn btn-primary">tie for second</button> <button id="rank1" accesskey="1" type="button" class="btn btn-primary">rank third</button>');
 			$('div#item3').html('<p>Second!</p>');
 		}
 		else if (rank1 == '1' && rank2 == '1') {
@@ -126,13 +126,13 @@ $(document).ready(function() {
 			$('[name=rank1]').val('1');
 			
 			$('div#item1').html('<p>First!</p>');
-			$('div#item3').html('<button id="tie3" type="button" class="btn btn-primary">tie for first</button> <button id="rank3" type="button" class="btn btn-primary">rank second</button>');
+			$('div#item3').html('<button id="tie3" accesskey="e" type="button" class="btn btn-primary">tie for first</button> <button id="rank3" accesskey="3" type="button" class="btn btn-primary">rank second</button>');
 		}
 		else if (rank2 == '0' && rank3 == '1') {
 			$('[name=rank1]').val('1');
 			
 			$('div#item1').html('<p>First!</p>');
-			$('div#item2').html('<button id="tie2" type="button" class="btn btn-primary">tie for first</button> <button id="rank2" type="button" class="btn btn-primary">rank second</button>');
+			$('div#item2').html('<button id="tie2" accesskey="w" type="button" class="btn btn-primary">tie for first</button> <button id="rank2" accesskey="2" type="button" class="btn btn-primary">rank second</button>');
 		}
 		else if (rank2 == '1' && rank3 == '1') {
 			$('[name=rank1]').val('1');
@@ -158,12 +158,12 @@ $(document).ready(function() {
 			$('[name=rank2]').val('1');
 			
 			$('div#item2').html('<p>First!</p>');
-			$('div#item3').html('<button id="tie3" type="button" class="btn btn-primary">tie for first</button> <button id="rank3" type="button" class="btn btn-primary">rank second</button>');
+			$('div#item3').html('<button id="tie3" accesskey="e" type="button" class="btn btn-primary">tie for first</button> <button id="rank3" accesskey="3" type="button" class="btn btn-primary">rank second</button>');
 		}
 		else if (rank1 == '0' && rank3 == '1') {
 			$('[name=rank2]').val('1');
 			
-			$('div#item1').html('<button id="tie1" type="button" class="btn btn-primary">tie for first</button> <button id="rank1" type="button" class="btn btn-primary">rank second</button>');
+			$('div#item1').html('<button id="tie1" accesskey="q" type="button" class="btn btn-primary">tie for first</button> <button id="rank1" accesskey="1" type="button" class="btn btn-primary">rank second</button>');
 			$('div#item2').html('<p>First!</p>');
 		}
 		else if (rank1 == '1' && rank3 == '1') {
@@ -189,13 +189,13 @@ $(document).ready(function() {
 		if (rank1 == '1' && rank2 == '0') {
 			$('[name=rank3]').val('1');
 			
-			$('div#item2').html('<button id="tie2" type="button" class="btn btn-primary">tie for first</button> <button id="rank2" type="button" class="btn btn-primary">rank second</button>');
+			$('div#item2').html('<button id="tie2" accesskey="w" type="button" class="btn btn-primary">tie for first</button> <button id="rank2" accesskey="2" type="button" class="btn btn-primary">rank second</button>');
 			$('div#item3').html('<p>First!</p>');
 		}
 		else if (rank1 == '0' && rank2 == '1') {
 			$('[name=rank3]').val('1');
 			
-			$('div#item1').html('<button id="tie1" type="button" class="btn btn-primary">tie for first</button> <button id="rank1" type="button" class="btn btn-primary">rank second</button>');
+			$('div#item1').html('<button id="tie1" accesskey="q" type="button" class="btn btn-primary">tie for first</button> <button id="rank1" accesskey="1" type="button" class="btn btn-primary">rank second</button>');
 			$('div#item3').html('<p>First!</p>');
 		}
 		else if (rank1 == '1' && rank2 == '1') {
@@ -218,9 +218,9 @@ $(document).ready(function() {
 		$('[name=rank2]').val('0');
 		$('[name=rank3]').val('0');
 		
-		$('div#item1').html('<button id="rank1" type="button" class="btn btn-primary">rank first</button>');
-		$('div#item2').html('<button id="rank2" type="button" class="btn btn-primary">rank first</button>');
-		$('div#item3').html('<button id="rank3" type="button" class="btn btn-primary">rank first</button>');
+		$('div#item1').html('<button id="rank1" accesskey="1" type="button" class="btn btn-primary">rank first</button>');
+		$('div#item2').html('<button id="rank2" accesskey="2" type="button" class="btn btn-primary">rank first</button>');
+		$('div#item3').html('<button id="rank3" accesskey="3" type="button" class="btn btn-primary">rank first</button>');
 		$('button#submit').attr('style', 'display: none;');
 	});
 });
